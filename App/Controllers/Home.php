27e7f120d8ad182f7b16fa008ce41603fffcf35d
CHENI-E <U24242255@utp.edu.pe>
2025-05-29 16:58:38 -0500

    [sacando el hola mundo}

--- App/Controllers/Home.php
+++ App/Controllers/Home.php
@@ -43,8 +43,8 @@ class Home
 
     public function storeEmail()
     {
-        echo "hola mundo";
-        return;
+        /* echo "hola mundo";
+        return; */
         session_start();
 
         // Inicializar la sesión si no existe
